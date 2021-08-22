3de3e2f4b42af4b0458da8b6e5616c93732ece85
Mat1234567890 <78577538+Mat1234567890@users.noreply.github.com>
2021-08-22 13:26:03 -0400

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,29 @@
+Canvas=document.getElementById("MyCanvas");
+ctx=Canvas.getContext("2d");
+color="Blue";
+ctx.beginPath();
+ctx.stokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200,200,40,0,2 * Math.PI);
+
+
+Canvas.addEventListener("mousedown",my_mousedown);
+
+function my_mousedown(e){
+color=document.getElementById("color").value;
+console.log(color);
+mouse_x= e.clientX -Canvas.offsetLeft;
+mouse_y = e.clientY - Canvas.offsetTop;
+console.log("X = " + mouse_x + " ,Y = " + mouse_y);
+circle(mouse_x , mouse_y);
+}
+function circle(mouse_x , mouse_y) {
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.arc(mouse_x, mouse_y, 40 ,0 , 2*Math.PI);
+ctx.stroke();
+}
+function clearArea(){
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
